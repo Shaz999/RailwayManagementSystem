4417c674bacf43addeb527c39e36438b108ec48c
Shaz Alam <99272333+Shaz999@users.noreply.github.com>
2024-09-28 12:26:59 +0530

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import mysql.connector
 
-ADMIN_API_KEY = 'your_admin_api_key'
-JWT_SECRET_KEY = 'your_jwt_secret_key'
+ADMIN_API_KEY = 'waMkHbbdpz-jd7FdS_f7RQ0dI6JtgrSc_p2TQcOg2zM'
+
+JWT_SECRET_KEY = 'rVfQHQlIzm7gG_jcI7vUG6ZUZjcp7fWqaLlRVZLriX8'
 
 def get_db_connection():
     return mysql.connector.connect(
